f4ca51d51834feba3052c40cf1c97a119158071b
Neuk Dmitriy <SadistDi@gmail.com>
2023-02-06 19:35:18 +0300

    input/output from the console

---
+++ src/main/kotlin/Manager.kt
@@ -0,0 +1,36 @@
+import data.Command
+
+object Manager {
+    private var command: String = ""
+    private var end = false
+    fun getCommand() {
+        while (!end) {
+            println("Введите команду:")
+            command = readln()
+            when (command) {
+                Command.START.commandText -> printHelloMessage()
+                Command.HELP.commandText -> printHelpMessage()
+                Command.END.commandText -> endProgram()
+                else -> printErrorMessage()
+            }
+        }
+    }
+
+    private fun endProgram() {
+        println("Работа программы завершена.\nХорошего дня!")
+        end = true
+    }
+
+    private fun printErrorMessage() {
+        println("Команда недоступна, для получения информации о командах используйте /help")
+    }
+
+    private fun printHelpMessage() {
+        println(Command.values().joinToString(separator = "\n") { "${it.commandText} - ${it.commandDescription}" })
+    }
+
+    private fun printHelloMessage() {
+        println("Добро пожаловать в приложение для парковки автомобилей!")
+    }
+
+}
